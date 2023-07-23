2f8266cb1c8a739603f03797f2701e9bb45e4191
Joseph Wang <jgwang@ad.unc.edu>
2023-07-22 22:08:20 -0500

    first commit

---
+++ src/test/java/dev/joseph/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.joseph.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
